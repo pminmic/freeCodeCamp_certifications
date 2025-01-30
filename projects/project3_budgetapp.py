b344c2deea496399e32bbcf8339bc8966a85d45d
Pau Minguet <pminmic@gmail.com>
2025-01-30 12:27:02 +0100

    project 3 progress

--- projects/project3_budgetapp.py
+++ projects/project3_budgetapp.py
@@ -1,4 +1,5 @@
 class Category:
+
     def __init__(self, name):
         self.name = name
     
@@ -14,19 +15,35 @@ class Category:
     def deposit(self, amount, action):
         self.amount = amount
         self.action = action
+        first_deposit = 0
         str_deposit = '\n'
-        if action == 'deposit':
+        if action == 'deposit' and first_deposit == 0:
             str_deposit += 'initial deposit'
-        
+            first_deposit += 1
+        else:
+            str_deposit += action
         
         return str_deposit
+    
+    def withdraw(self, amount, action):
+        self.amount = amount
+        self.action = action
+        pass
+
+    def get_balance(self):
+        pass
+    
+    def transfer(self, amount, category):
+        pass
+
+    def check_funds(self):
 
 
     
-
 def create_spend_chart(categories):
-    pass
+    str_chart = 'Percentage spent by category' + '\n'
+
 
 food = Category('Food')
-#.deposit(1000, 'deposit')
+food.deposit(1000, 'deposit')
 print(food)
