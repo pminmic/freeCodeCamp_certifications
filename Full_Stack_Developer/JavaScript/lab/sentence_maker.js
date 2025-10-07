b5d8f8c78195c8516a9b7e295c0da356f79b80b4
Pau Minguet <pminmic@gmail.com>
2025-10-07 19:18:16 +0200

    Conceptos básicos JS

---
+++ Full_Stack_Developer/JavaScript/lab/sentence_maker.js
@@ -0,0 +1,25 @@
+let adjective = "envious";
+let noun = "enthusiasm";
+let verb = "kill";
+let place = "Catalonia";
+let adjective2 = "simplistic";
+let noun2 = "hair";
+
+const firstStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " +
+  noun2 + ". The " + noun + " lived in a " + place + " and had " + adjective2 + 
+  " nostrils that blew fire when it was " + verb + ".";
+
+console.log("First story: " + firstStory);
+
+adjective = "slow";
+noun = "tree";
+verb = "eating";
+place = "gulag";
+adjective2 = "fast";
+noun2 = "friend";
+
+const secondStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " +
+  noun2 + ". The " + noun + " lived in a " + place + " and had " + adjective2 + 
+  " nostrils that blew fire when it was " + verb + ".";
+
+console.log("Second story: " + secondStory);
---
+++ Full_Stack_Developer/JavaScript/lab/trivia_bot.js
@@ -0,0 +1,23 @@
+console.log("Hello! I'm your coding fun fact guide!");
+
+let botName = "Manel";
+let botLocation = "Cuba";
+let favoriteLanguage = "JavaScript";
+
+const greeting = "My name is " + botName + " and I live on " + botLocation + ".";
+console.log(greeting);
+
+const language = "My favorite programming language is " + favoriteLanguage + ".";
+console.log(language);
+
+let codingFact = "I don't know how to use " + favoriteLanguage + "!";
+console.log(codingFact);
+
+codingFact = "But I'm very excited to learn about " + favoriteLanguage + "!";
+console.log(codingFact);
+
+codingFact = "I don't know more facts about " + favoriteLanguage + ".";
+console.log(codingFact);
+
+const goodbye = "It was fun sharing these facts with you. Goodbye! - " + botName + " from " + botLocation + ".";
+console.log(goodbye);
